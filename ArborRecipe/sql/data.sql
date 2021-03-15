20fd944e91094f0b3730434c92d4813fe91d8146
rudhshah <rudhshah@umich.edu>
2021-03-15 13:52:22 -0400

    Added Sample data and changed schema

---
+++ ArborRecipe/sql/data.sql
@@ -0,0 +1,6 @@
+INSERT INTO ingrediant_table(ingrediant,recipe_id)
+VALUES('Bread',1),('Avacado',1)('Beans',2)('Salsa',2)('Cheese',2)('Tortios',2);
+
+INSERT INTO recipe(ingrediant,recipe_id)
+VALUES(1,'Avacado Toast','1) Toast the bread. 2) Cut the Avacado. 3) spread the Avacado on the Toast.', 5),
+(2, 'Bean Burrito', '1) Take a Tortios, 2) Spread Salsa on the Tortios. 3) Add Beans. 4) Add Cheese 5) Microvave it for 2 mins', 10);
--- ArborRecipe/sql/schema.sql
+++ ArborRecipe/sql/schema.sql
@@ -1,13 +1,13 @@
-CREATE TABLE ingrediants (
+CREATE TABLE ingrediant_table (
     ingrediants VARCHAR(20) NOT NULL,
     recipe_id INTEGER NOT NULL,
     PRIMARY KEY(ingrediants)
 );
 
 CREATE TABLE recipie (
-    instruction VARCHAR(100) NOT NULL,
-    recipe_id INTEGER NOT NULL,
+	recipe_id INTEGER NOT NULL,
     recipe_name VARCHAR(20) NOT NULL,
+    instruction VARCHAR(500) NOT NULL,
     cooking_time INTEGER NOT NULL,
     PRIMARY KEY(recipe_id)
 );
