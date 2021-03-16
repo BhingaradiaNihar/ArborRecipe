5a4505e74026f02237464010d3f08295b727d1bc
BhingaradiaNihar <niharbhingaradia@gmail.com>
2021-03-16 00:42:19 -0400

    need to fix passing variables

--- ArborRecipe/js/search.jsx
+++ ArborRecipe/js/search.jsx
@@ -2,6 +2,7 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
+
 class Search extends React.Component {
   /* Display number of likes and like/unlike button for one post
    * Reference on forms https://facebook.github.io/react/docs/forms.html
