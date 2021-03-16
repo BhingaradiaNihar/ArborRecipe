8b01fa0e59a4db1341a44f4524dd380169fef366
arenbecl <carenber@umich.edu>
2021-03-15 23:08:07 -0400

    Update data.sql

--- ArborRecipe/sql/data.sql
+++ ArborRecipe/sql/data.sql
@@ -1,6 +1,18 @@
 INSERT INTO ingredient_table(ingredients,recipe_id)
-VALUES('Bread',"{'1'}"),('Avacado',"{'1'}"),('Beans',"{'2'}"),('Salsa',"{'2'}"),('Cheese',"{'2'}"),('Tortios',"{'2'}");
+VALUES('Bread',"{'1'}"),('Avacado',"{'1'}"),('Beans',"{'2'}"),('Salsa',"{'2'}"),('Cheese',"{'2'}"),('Tortilla',"{'2'}"), 
+('Olive oil', "{'3'}"),('Onion', "{'3'}"),('Garlic', "{'3'}"),('Ginger', "{'3'}"),('Curry powder', "{'3'}"),
+('Spinach', "{'3'}"),('Tomato Sauce', "{'3'}"),('Chickpeas', "{'3'}"),;
 
-INSERT INTO recipe(recipe_id,recipe_name,instruction,cooking_time)
-VALUES(1,'Avacado Toast','1) Toast the bread. 2) Cut the Avacado. 3) spread the Avacado on the Toast.', 5),
-(2, 'Bean Burrito', '1) Take a Tortios, 2) Spread Salsa on the Tortios. 3) Add Beans. 4) Add Cheese 5) Microvave it for 2 mins', 10);
+INSERT INTO recipe(recipe_id,recipe_name,ingredients,instruction,prep_time)
+VALUES
+(1,'Avacado Toast','1 Slice of bread;1 Avocado /n','1) Toast the bread;2) Cut the Avacado;3) spread the Avacado on the Toast', 5),
+(2, 'Bean Burrito','1 tortilla;1 cup of black beans;1/4 cup of salsa', '1) Take a tortilla, 2) Spread Salsa on the tortilla. 3) Add Beans. 4) Add Cheese 5) Microvave it for 2 mins', 10),
+(3, 'Curried Chickpeas Spinach', '2 Tbsp olive oil;1 onion;2 cloves of garlic;1 inch fresh ginger;1 1/2 Tbsp curry powder;8 oz. spinach;15 oz tomato sauce;15 oz. chickpeas','1) Dice the onion, mince the garlic, and grate the ginger on a small holed cheese grater. Sauté the onion, garlic, and ginger in a large skillet with the olive oil over medium heat until the onions have softened (3-5 minutes).;2) Add the curry powder and continue to sauté with the onion mixture for one minute more. Add about 1/4 cup of water to the skillet along with the fresh spinach and continue to sauté until the spinach has wilted. The water will help steam and wilt the spinach (no water needed if using frozen spinach).;3) Drain and rinse the chickpeas. Add them to the skillet along with the tomato sauce. Stir well to distribute the spices in the sauce and heat through (5 minutes). Serve over rice or with bread.;Source: https://www.budgetbytes.com/curried-chickpeas-spinach/',25),
+(4,,,,),
+(5,,,,),
+(6,,,,),
+(7,,,,),
+(8,,,,),
+(9,,,,),
+(10,,,,),
+;
