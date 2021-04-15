0fb17ed2e2b9fd699cf510c9e6dfdd69d19f82a9
BhingaradiaNihar <niharbhingaradia@gmail.com>
2021-04-14 21:23:59 -0400

    restriction works

--- ArborRecipe/api/recipe.py
+++ ArborRecipe/api/recipe.py
@@ -5,24 +5,34 @@ from ArborRecipe.api.invalid_usage import InvalidUsage
 from flask import jsonify
 from flask import request
 import ast
+import re
 import json
 
 # fix lower case letter
 
+
+def find_word(string1, match_list):
+    for ingr in match_list:
+        if(re.search("("+ ingr +")" , string1, re.IGNORECASE)):
+            return True
+    return False
+
+
 @ArborRecipe.app.route('/api/i', methods=["GET"])
 def get_recipe():
 
-    print("get recipe")
+    #print("get recipe")
     ingredients = [] 
     # saves sets of recipe id
     recipe_id_sets = []
     recipes_id = set()
     recipes_text = {}
 
-    restr = request.args.get('restr')   
-    print(request.args.get('ingredients'))
+    restr = request.args.get('restrictions').lower()
+    print("restr" , restr)
+    #print(request.args.get('ingredients'))
     ingredients = request.args.get('ingredients').split(",")
-    print(ingredients)
+    #print(ingredients)
     connection = ArborRecipe.model.get_db()
     for ingredient in ingredients:
         cur = connection.execute(
@@ -33,14 +43,25 @@ def get_recipe():
         recipe_id_sets.append(ast.literal_eval(cur["recipe_id"]))
     
 
-    print("recipe_id_sets", recipe_id_sets) 
+    #print("recipe_id_sets", recipe_id_sets) 
     # find recipes 
     for set_id in recipe_id_sets:
-        print(type(set_id), type(recipes_id))
+        #print(type(set_id), type(recipes_id))
         recipes_id = recipes_id | set_id
 
     number = 0 
+    print(recipes_id)
+    restriction_data = {
+            "dairy" : ["milk", "greekyogurt", "yogurt", "sourcream", "cream", "alfredo", "mayonnaise", "mozzarella", "parmesan", "brie", "bleu", "feta", "cottage cheese", "whipping cream"],
+            "egg" : ["egg", "egg white", "egg yolk"],
+            "peanut" : ["peanut", "peanut butter"],
+            "shellfish" : ["crab", "lobster", "shrimp", "oyster", "clam", "scallop"],
+            "vegetarian" : ["fish", "tuna", "beef", "ham", "salmon", "cod", "pork", "bacon", "crab", "lobster", "lamb", "duck", "goat", "veal", "turkey"],
+            "halal" : ["pork", "bacon"]
+            }
 
+    restriction_ingredients = [] if restr=="" else restriction_data[restr]
+    print("res_ing",  restriction_ingredients)
 
     res_list = []
     for recipe in recipes_id:
@@ -48,6 +69,11 @@ def get_recipe():
             "SELECT * FROM recipe_table WHERE recipe_id = ?", [recipe]
         ).fetchone()
         
+
+    
+        if(find_word(cur2["ingredients"], restriction_ingredients)):
+            continue
+
         temp = {
             "recipe_id"  : cur2["recipe_id" ],
             "recipe_name" :  cur2["recipe_name"],
